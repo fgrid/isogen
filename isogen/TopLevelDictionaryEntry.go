d419a0cc8f36a6589d101f493f525686de69b98d
Stephan Heinze <stephan.heinze.private@gmail.com>
2016-04-21 00:23:11 +0200

    feat: generate Amounts with value and currency

--- isogen/TopLevelDictionaryEntry.go
+++ isogen/TopLevelDictionaryEntry.go
@@ -33,11 +33,25 @@ type {{.Name}} struct {
 
 {{range .MessageElement}}{{.Access $.Name}}{{end}}
 `
+	amountTemplate = `package {{.PackageName}}
+
+// {{ replace .Definition "\n" "\n\t// " -1}}
+type {{.Name}} struct {
+	Value string ` + "`" + `xml:",chardata"` + "`" + `
+	Currency string ` + "`" + `xml:"Ccy"` + "`" + `
+}
+
+func New{{.Name}} (value, currency string) *{{.Name}} {
+	return &{{.Name}}{Value: value, Currency: currency}
+}
+
+`
 )
 
 var (
 	simpleType  *template.Template
 	complexType *template.Template
+	amount      *template.Template
 )
 
 func init() {
@@ -51,6 +65,9 @@ func init() {
 	if complexType, err = template.New("complexType").Funcs(funcMap).Parse(complexTypeTemplate); err != nil {
 		log.Fatalf("could not compile complexTypeTemplate - %s", err.Error())
 	}
+	if amount, err = template.New("amount").Funcs(funcMap).Parse(amountTemplate); err != nil {
+		log.Fatalf("could not compile amountTemplate - %s", err.Error())
+	}
 }
 
 func (t *TopLevelDictionaryEntry) Analyse() {
@@ -60,31 +77,37 @@ func (t *TopLevelDictionaryEntry) Analyse() {
 func (t *TopLevelDictionaryEntry) Generate(packageName string) {
 	t.PackageName = packageName
 	switch t.XSIType {
-	case "iso20022:Amount", "iso20022:Binary", "iso20022:CodeSet",
+	case "iso20022:Binary", "iso20022:CodeSet",
 		"iso20022:Date", "iso20022:DateTime", "iso20022:ExternalSchema",
 		"iso20022:IdentifierSet", "iso20022:Indicator", "iso20022:Quantity",
 		"iso20022:Rate", "iso20022:Text", "iso20022:Time",
 		"iso20022:UserDefined", "iso20022:Year", "iso20022:YearMonth":
 		t.GenerateSimpleType()
 	case "iso20022:MessageComponent", "iso20022:ChoiceComponent":
-		t.GenerateComplexType(typeMap)
+		t.GenerateComplexType()
+	case "iso20022:Amount":
+		t.GenerateAmount()
 	}
 }
 
 func (t *TopLevelDictionaryEntry) GenerateSimpleType() {
-	f, err := os.OpenFile(t.Name+".go", os.O_RDWR|os.O_CREATE, 0660)
-	if err != nil {
-		log.Fatalf("can not create file %s - %s", t.Name+".go", err.Error())
-	}
-	defer f.Close()
-	simpleType.Execute(f, t)
+	t.GenerateWithTemplate(simpleType)
 }
 
-func (t *TopLevelDictionaryEntry) GenerateComplexType(typeMap TypeMap) {
+func (t *TopLevelDictionaryEntry) GenerateComplexType() {
+	t.GenerateWithTemplate(complexType)
+}
+
+func (t *TopLevelDictionaryEntry) GenerateAmount() {
+	log.Printf("generating amount %s", t.Name)
+	t.GenerateWithTemplate(amount)
+}
+
+func (t *TopLevelDictionaryEntry) GenerateWithTemplate(tmpl *template.Template) {
 	f, err := os.OpenFile(t.Name+".go", os.O_RDWR|os.O_CREATE, 0660)
 	if err != nil {
 		log.Fatalf("can not create file %s - %s", t.Name+".go", err.Error())
 	}
 	defer f.Close()
-	complexType.Execute(f, t)
+	tmpl.Execute(f, t)
 }
